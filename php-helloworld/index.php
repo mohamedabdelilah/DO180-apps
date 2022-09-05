c7578d9a2e91d92ece1b7961d637310bdb9b7729
Student User <student@workstation.lab.example.com>
2022-09-05 14:10:42 -0400

    another chnage

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,7 +2,7 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print " A change is coming \n ";
 print " Another change ! \n";
-print " What if I tell you that Another change might come !!!!  \n;
+print " What if I tell you that Another change might come !!!!  \n";
 ?>
 
 
